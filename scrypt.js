3c9d5acfb88f393e3dcefe8be38f70775f06b060
jhonatanGuingo <jhonatan.amaral@engenharia.ufjf.br>
2023-04-02 19:31:48 -0300

    seletor e verificao

---
+++ scrypt.js
@@ -0,0 +1,48 @@
+function selecionarPrato(seletor){    
+
+    const botaoSelecionadoAnteriormente = document.querySelector('.container-comidas .borda');
+    
+    if ( botaoSelecionadoAnteriormente !== null ){
+        botaoSelecionadoAnteriormente.classList.remove('borda');
+    }
+    seletor.classList.add('borda');
+    verificarSelecao();
+}
+
+function selecionarBebida(seletor){    
+
+    const botaoSelecionadoAnteriormente = document.querySelector('.container-bebidas .borda');
+    
+    if ( botaoSelecionadoAnteriormente !== null ){
+        botaoSelecionadoAnteriormente.classList.remove('borda');
+    }
+    seletor.classList.add('borda');
+    verificarSelecao();
+}
+
+
+function selecionarEntrada(seletor){    
+
+    const botaoSelecionadoAnteriormente = document.querySelector('.container-entradas .borda');
+    
+    if ( botaoSelecionadoAnteriormente !== null ){
+        botaoSelecionadoAnteriormente.classList.remove('borda');
+    }
+    seletor.classList.add('borda');
+    verificarSelecao();
+}
+
+function verificarSelecao(){
+
+    const selecao1 = document.querySelector('.container-comidas .borda' );
+    const selecao2 = document.querySelector('.container-bebidas .borda' );
+    const selecao3 = document.querySelector('.container-entradas .borda' );
+
+    if (selecao1 !== null  && selecao2 !== null && selecao3 !== null){
+        const fechar = document.querySelector('.rodape .fechar-pedido');
+        fechar.innerHTML = "Fechar Pedido";
+    }
+    
+}
+
+ 
